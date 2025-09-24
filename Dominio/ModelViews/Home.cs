c700d3a4a570704760afce572a49b0c3a34c5856
Deyvisson-del <deyvissonrafael018@gmail.com>
2025-09-24 11:49:51 -0300

    Criando rota Home respondendo por JSON

---
+++ Dominio/ModelViews/Home.cs
@@ -0,0 +1,6 @@
+namespace minimal_api.Dominio.ModelViews;
+public struct Home
+{
+    public string Mensagem { get => "Bem-Vindo a API de Veiculos"; }
+    public string Doc { get => "/swagger"; }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using minimal_api.Dominio.Dtos;
 using minimal_api.Dominio.Interfaces;
 using minimal_api.Dominio.Servicos;
 using Microsoft.AspNetCore.Mvc;
+using minimal_api.Dominio.ModelViews;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +18,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 builder.Services.AddDbContext<DbContexto>( options =>
     {
@@ -39,24 +41,29 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Deyvisson", "William", "Guilherme", "Eduardo", "Cleber", "Carla", "Lucas", "Kauan", "Cauan", "Luis"
-};
+//var summaries = new[]
+//{
+//    "Deyvisson", "William", "Guilherme", "Eduardo", "Cleber", "Carla", "Lucas", "Kauan", "Cauan", "Luis"
+//};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 20).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+
+app.MapGet("/", () => Results.Json(new Home())); 
+
+
+//app.MapGet("/weatherforecast", () =>
+//{
+//    var forecast = Enumerable.Range(1, 20).Select(index =>
+//        new WeatherForecast
+//        (
+//            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+//            Random.Shared.Next(-20, 55),
+//            summaries[Random.Shared.Next(summaries.Length)]
+//        ))
+//        .ToArray();
+//    return forecast;
+//})
+//.WithName("GetWeatherForecast");
+
 
 
 app.MapPost("/login", ([FromBody]LoginDTO loginDTO, IAdministradorServico administradorServico) =>
