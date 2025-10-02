e4e015c5b8a72fa1dcfd2575faafdf7f884fec21
Deyvisson-del <deyvissonrafael018@gmail.com>
2025-10-02 16:43:33 -0300

    Teste de serviço administrador(Erro CS0103 e CS1061)

--- obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
+++ obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b887dbb13b5f30450a0cbaf5250a7498745a8349")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+300a91e0dc0d9cf36e96a4963b25fc9cac18d565")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
